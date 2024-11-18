5ff074480ddc12e5ee840e22ef6a5a2fe954f724
yenifer leon <visitante@gmail.com>
2024-11-18 09:13:47 -0500

    Animal

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -1,5 +1,9 @@
 package logicaNegocio;
 
 public abstract class Animal {
-    // TODO: Aquí va tu código
+ protected TipoHabitat habitad;
+  
+ public abstract String emitirSonido();
+
+public abstract String obtenerDieta(); 
 }
